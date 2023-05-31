f9351fb41e8a252c02dacc8441db9899fe4ef6fb
maxgroenendk1 <max.groenendijk@student.hu.nl>
2023-05-31 21:16:55 +0200

    veranderingen in de klas

---
+++ src/main/model/Action.java
@@ -0,0 +1,11 @@
+package main.model;
+
+public class Action {
+    private String name;
+    private String omschrijving;
+    private int time;
+
+    public Action(){
+
+    }
+}
---
+++ src/main/model/main.java
@@ -0,0 +1,8 @@
+package main.model;
+
+public class main {
+    public static void main(String[] args){
+        IngredientRepository IR = new Ingredient.FileBasedIngredientRepository();
+
+    }
+}
